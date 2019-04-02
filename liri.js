c42ccea72c6cde3a42d50c3ccfbc1cd445bc1c3c
Babis <cwbabis@gmail.com>
2019-04-01 19:52:19 -0700

    init

---
+++ liri.js
@@ -0,0 +1,5 @@
+require("dotenv").config();
+var keys = require("./keys.js");
+var spotify = new Spotify(keys.spotify);
+
+var commands = do-what-it-says, concert-this, spotify-this-song, movie-this
