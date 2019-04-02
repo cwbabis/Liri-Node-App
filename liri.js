8b8891d6c86bdbd1981997301456754505f71558
Babis <cwbabis@gmail.com>
2019-04-01 20:06:17 -0700

    commands added

--- liri.js
+++ liri.js
@@ -2,4 +2,21 @@ require("dotenv").config();
 var keys = require("./keys.js");
 var spotify = new Spotify(keys.spotify);
 
-var commands = do-what-it-says, concert-this, spotify-this-song, movie-this
+var inputString = process.argv;
+var command = inputString[2];
+
+if (command === "concert-this") {
+
+}
+else if (command === "spotify-this-song"){
+
+}
+else if (command === "movie-this"){
+
+}
+else if (command === "do-what-it-says"){
+
+}
+else {
+    command = "Not a recognized command"
+}
