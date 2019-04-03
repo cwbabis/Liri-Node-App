fd489af302fd55099e4e8668cbdffe6af3b5cd99
Babis <cwbabis@gmail.com>
2019-04-02 20:21:36 -0700

    prepped command 2. redoprep at home desktop. .env, npm install spotify etc

--- liri.js
+++ liri.js
@@ -47,6 +47,11 @@ if (command === "concert-this") {
 }
 
 else if (command === "spotify-this-song"){
+// command to display Artist(s)
+//The song's name
+//A preview link of the song from Spotify
+//The album that the song is from
+
 
 }
 else if (command === "movie-this"){
