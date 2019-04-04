de2cb3e7d396b4a242908a22bb4d68095967ee7c
Babis <cwbabis@gmail.com>
2019-04-04 15:48:10 -0700

    OMDB almost done. need rotten tomatoes score and get default output data to work

--- liri.js
+++ liri.js
@@ -4,7 +4,7 @@ require('node-spotify-api');
 var axios = require("axios");
 var moment = require('moment');
 var keys = require("./keys.js");
-var spotify = new Spotify(keys.spotify);
+/* var spotify = new Spotify(keys.spotify); */
 
 
 
@@ -13,22 +13,22 @@ var command = inputString[2];
 
 
 if (command === "concert-this") {
-    var artist = inputString[3];
-    //%20
-    
-    axios
+  var artist = inputString[3];
+  //%20
+
+  axios
     .get("https://rest.bandsintown.com/artists/" + artist + "/events?app_id=codingbootcamp")
-    .then(function(response) {
+    .then(function (response) {
       var returningObject = response.data
-      for (i = 0; i < returningObject.length; i++){
-      console.log(response.data[i].venue.name);
-      console.log(response.data[i].venue.city);
-      console.log(moment(response.data[i].datetime).format("MM/DD/YYYY"));
-      console.log("=====================================")
+      for (i = 0; i < returningObject.length; i++) {
+        console.log(response.data[i].venue.name);
+        console.log(response.data[i].venue.city);
+        console.log(moment(response.data[i].datetime).format("MM/DD/YYYY"));
+        console.log("=====================================")
       }
     })
-  
-    .catch(function(error) {
+
+    .catch(function (error) {
       if (error.response) {
         // The request was made and the server responded with a status code
         // that falls out of the range of 2xx
@@ -47,28 +47,78 @@ if (command === "concert-this") {
     });
 }
 
-else if (command === "spotify-this-song"){
+else if (command === "spotify-this-song") {
   var song = inputString[3];
-  spotify.search({ type: 'track', query: song }, function(err, data) {
+  spotify.search({ type: 'track', query: song }, function (err, data) {
     if (err) {
       return console.log('Error occurred: ' + err);
     }
-   
-  console.log(data); 
+
+    console.log(data);
   });
-// command to display Artist(s)
-//The song's name
-//A preview link of the song from Spotify
-//The album that the song is from
+  // command to display Artist(s)
+  //The song's name
+  //A preview link of the song from Spotify
+  //The album that the song is from
+}
+else if (command === "movie-this") {
+
+  var movie = inputString[3];
+  axios.get("http://www.omdbapi.com/?t=" + movie + "&y=&plot=short&apikey=trilogy").then(
+    function (response) {
+      // This will output the following information to your terminal/bash window:
+      console.log("==============================");
+      // * Title of the movie.
+      console.log("Title: " + response.data.Title);
+      // * Year the movie came out.
+      console.log("Release Year: " + response.data.Year);
+      // * IMDB Rating of the movie.
+      console.log("IMDB Rating: " + response.data.imdbRating);
+      // * Rotten Tomatoes Rating of the movie.
+      console.log("Rotten Tomatoes: " + response.data.tomatoRating);
+      // * Country where the movie was produced.
+      console.log("Produced in: " + response.data.Country);
+      // * Language of the movie.
+      console.log("Language: " + response.data.Language);
+      // * Plot of the movie.
+      console.log("Plot: " + response.data.Plot);
+      // * Actors in the movie.
+      console.log("Actors: " + response.data.Actors);
+
+      if (inputString[3] = inputString[""]) {
+        axios.get("http://www.omdbapi.com/?t=" + "Mr. Nobody" + "&y=&plot=short&apikey=trilogy").then(
+          function (response) {
+            // This will output the following information to your terminal/bash window:
+            console.log("==============================");
+            // * Title of the movie.
+            console.log("Title: " + response.data.Title);
+            // * Year the movie came out.
+            console.log("Release Year: " + response.data.Year);
+            // * IMDB Rating of the movie.
+            console.log("IMDB Rating: " + response.data.imdbRating);
+            // * Rotten Tomatoes Rating of the movie.
+            console.log("Rotten Tomatoes: " + response.data.tomatoRating);
+            // * Country where the movie was produced.
+            console.log("Produced in: " + response.data.Country);
+            // * Language of the movie.
+            console.log("Language: " + response.data.Language);
+            // * Plot of the movie.
+            console.log("Plot: " + response.data.Plot);
+            // * Actors in the movie.
+            console.log("Actors: " + response.data.Actors);
+          });
+      };
+
+    }
+  );
+
+
 
 
 }
-else if (command === "movie-this"){
-
-}
-else if (command === "do-what-it-says"){
+else if (command === "do-what-it-says") {
 
 }
 else {
-    command = "Not a recognized command"
+  command = "Not a recognized command"
 }
