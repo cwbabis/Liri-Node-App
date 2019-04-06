e66552bc5487eec51faaf9b300edd8cdde5ab449
Babis <cwbabis@gmail.com>
2019-04-06 12:27:46 -0700

    cleaned up code

--- liri.js
+++ liri.js
@@ -1,17 +1,14 @@
 require("dotenv").config();
 var Spotify = require('node-spotify-api');
-
 var fs = require("fs");
 var axios = require("axios");
 var moment = require('moment');
 var keys = require("./keys.js");
 var spotify = new Spotify(keys.spotify);
 
-
-
 var inputString = process.argv;
 var command = inputString[2];
-var userInput = inputString[3];
+var userInput = inputString.splice(3).join(" ");
 
 if (command === "do-what-it-says") {
   fs.readFile("random.txt", "utf8", function (error, data) {
@@ -26,130 +23,63 @@ if (command === "do-what-it-says") {
     }
   })
 }
-function control(){
-if (command === "concert-this") {
-  var artist = userInput;
-  //%20
+function control() {
+  if (command === "concert-this") {
+    var artist = userInput;
 
-  axios
-    .get("https://rest.bandsintown.com/artists/" + artist + "/events?app_id=codingbootcamp")
-    .then(function (response) {
-      var returningObject = response.data
-      for (i = 0; i < returningObject.length; i++) {
-        console.log(response.data[i].venue.name);
-        console.log(response.data[i].venue.city);
-        console.log(moment(response.data[i].datetime).format("MM/DD/YYYY"));
-        console.log("=====================================")
+    axios
+      .get("https://rest.bandsintown.com/artists/" + artist + "/events?app_id=codingbootcamp")
+      .then(function (response) {
+        var returningObject = response.data
+        for (i = 0; i < returningObject.length; i++) {
+          console.log(response.data[i].venue.name);
+          console.log(response.data[i].venue.city);
+          console.log(moment(response.data[i].datetime).format("MM/DD/YYYY"));
+          console.log("==========================================")
+        }
+      })
+      .catch(function (error) {
+        if (error.response) {
+          console.log(error.response.data);
+          console.log(error.response.status);
+          console.log(error.response.headers);
+        } else if (error.request) {
+          console.log(error.request);
+        } else {
+          console.log("Error", error.message);
+        }
+        console.log(error.config);
+      });
+  }
+  else if (command === "spotify-this-song") {
+    if (!userInput) {
+      userInput = "The Sign Ace of Base"
+    }
+    spotify.search({ type: 'track', query: userInput }, function (err, data) {
+      if (err) {
+        return console.log('Error occurred: ' + err);
       }
-    })
-
-    .catch(function (error) {
-      if (error.response) {
-        // The request was made and the server responded with a status code
-        // that falls out of the range of 2xx
-        console.log(error.response.data);
-        console.log(error.response.status);
-        console.log(error.response.headers);
-      } else if (error.request) {
-        // The request was made but no response was received
-        // `error.request` is an object that comes back with details pertaining to the error that occurred.
-        console.log(error.request);
-      } else {
-        // Something happened in setting up the request that triggered an Error
-        console.log("Error", error.message);
-      }
-      console.log(error.config);
+      console.log("======================================" + "\n========== Song Information ==========" + "\n======================================" + "\n" + data.tracks.items[0].artists[0].name + "\n" + data.tracks.items[0].name + "\n" + data.tracks.items[0].preview_url + "\n" + data.tracks.items[0].album.name + "\n======================================")
     });
-}
-
-else if (command === "spotify-this-song") {
-  var song = userInput;
-  spotify.search({ type: 'track', query: song }, function (err, data) {
-    if (err) {
-      return console.log('Error occurred: ' + err);
+  }
+  else if (command === "movie-this") {
+    var movie = userInput;
+    if (!userInput) {
+      axios.get("http://www.omdbapi.com/?t=" + "Mr. Nobody" + "&y=&plot=short&apikey=trilogy").then(
+        function (response) {
+          console.log("=======================================" + "\n========== Movie Information ==========" + "\n=======================================" + "\nTitle: " + response.data.Title + "\nRelease Year: " + response.data.Year + "\nIMDB Rating: " + response.data.imdbRating + "\nRotten Tomatoes: " + response.data.Ratings[1].Value + "\nProduced in: " + response.data.Country + "\nLanguage: " + response.data.Language + "\nPlot: " + response.data.Plot + "\nActors: " + response.data.Actors + "\n=======================================");
+        });
     }
-    console.log(data.tracks.items[0].artists[0].name);
-    console.log(data.tracks.items[0].name);
-    console.log(data.tracks.items[0].preview_url);
-    console.log(data.tracks.items[0].album.name);
-    console.log("===================================");
-
-
-  });
-  // command to display Artist(s)
-  //The song's name
-  //A preview link of the song from Spotify
-  //The album that the song is from
-}
-else if (command === "movie-this") {
-  /*   var nodeArgs = process.argv;
-  
-    // Create an empty variable for holding the movie name
-    var movieName = "";
-    
-    // Loop through all the words in the node argument
-    // And do a little for-loop magic to handle the inclusion of "+"s
-    for (var i = 2; i < nodeArgs.length; i++) {
-    
-      if (i > 2 && i < nodeArgs.length) {
-        movieName = movieName + "+" + nodeArgs[i];
-      }
-      else {
-        movieName += nodeArgs[i];
-    
-      }
-    } */
-
-  var movie = userInput;
-  axios.get("http://www.omdbapi.com/?t=" + movie + "&y=&plot=short&apikey=trilogy").then(
-    function (response) {
-      // This will output the following information to your terminal/bash window:
-      console.log("==============================");
-      // * Title of the movie.
-      console.log("Title: " + response.data.Title);
-      // * Year the movie came out.
-      console.log("Release Year: " + response.data.Year);
-      // * IMDB Rating of the movie.
-      console.log("IMDB Rating: " + response.data.imdbRating);
-      // * Rotten Tomatoes Rating of the movie.
-      console.log("Rotten Tomatoes: " + response.data.tomatoRating);
-      // * Country where the movie was produced.
-      console.log("Produced in: " + response.data.Country);
-      // * Language of the movie.
-      console.log("Language: " + response.data.Language);
-      // * Plot of the movie.
-      console.log("Plot: " + response.data.Plot);
-      // * Actors in the movie.
-      console.log("Actors: " + response.data.Actors);
-
-      if (inputString[3] = inputString[""]) {
-        axios.get("http://www.omdbapi.com/?t=" + "Mr. Nobody" + "&y=&plot=short&apikey=trilogy").then(
-          function (response) {
-            // This will output the following information to your terminal/bash window:
-            console.log("==============================");
-            // * Title of the movie.
-            console.log("Title: " + response.data.Title);
-            // * Year the movie came out.
-            console.log("Release Year: " + response.data.Year);
-            // * IMDB Rating of the movie.
-            console.log("IMDB Rating: " + response.data.imdbRating);
-            // * Rotten Tomatoes Rating of the movie.
-            console.log("Rotten Tomatoes: " + response.data.tomatoRating);
-            // * Country where the movie was produced.
-            console.log("Produced in: " + response.data.Country);
-            // * Language of the movie.
-            console.log("Language: " + response.data.Language);
-            // * Plot of the movie.
-            console.log("Plot: " + response.data.Plot);
-            // * Actors in the movie.
-            console.log("Actors: " + response.data.Actors);
-          });
-      };
+    else {
+      axios.get("http://www.omdbapi.com/?t=" + movie + "&y=&plot=short&apikey=trilogy").then(
+        function (response) {
+          console.log("=======================================" + "\n========== Movie Information ==========" + "\n=======================================" + "\nTitle: " + response.data.Title + "\nRelease Year: " + response.data.Year + "\nIMDB Rating: " + response.data.imdbRating + "\nRotten Tomatoes: " + response.data.Ratings[1].Value + "\nProduced in: " + response.data.Country + "\nLanguage: " + response.data.Language + "\nPlot: " + response.data.Plot + "\nActors: " + response.data.Actors + "\n=======================================");
+        }
+      );
     }
-  );
-}
-else {
-  command = "Not a recognized command"
-}
+  }
+  else {
+    command = "Not a recognized command"
+  }
 }
 control();
